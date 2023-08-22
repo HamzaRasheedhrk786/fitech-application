8bc7133ff5838b6f3dbcfdf8c69377f3106e9cba
HamzaRasheedhrk786 <54620717+HamzaRasheedhrk786@users.noreply.github.com>
2023-08-22 23:03:12 +0500

    Update dbConfig.js

--- CONFIG/dbConfig.js
+++ CONFIG/dbConfig.js
@@ -1,3 +1,6 @@
+// module.exports ={
+//     mongodbonline:"mongodb+srv://fitnessApp:fitnessApp@cluster0.8yfdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+// }
 module.exports ={
     mongodbonline:"mongodb+srv://dbuser:dbuser@cluster0.v8xhw1c.mongodb.net/"
 }
